2ec0f890b5788ebd408849d1469cc2943347bfff
Dulce <dulcefrancelispc@gmail.com>
2018-03-27 16:31:59 -0400

    complete desing

--- js/main.js
+++ js/main.js
@@ -1,27 +1,8 @@
 $(document).ready(function() {
-  M.updateTextFields();
-  $('input.autocomplete').autocomplete({
-    data: {
-      "pediatría": null,
-      "Cardiologia": null,
-      "Endocrinologia": null,
-      "Odontologia":null,
-      "Neurologia":null,
-      "Gastroenterologia":null,
-      "Pulmonologia":null,
-      "Hermatologia":null,
-      "Nutriologia":null,
-      "Oftalmologia":null,
-      "Traumatologia":null,
-      "Urologia":null,
-      "Ginecologia":null,
-      "Dermatologia":null,
-    },
-  });
+  
 });
 
-function prueba(){
+/*function prueba(){
 let cojer = document.getElementsByClassName('range-field').innerHTML;
 document.getElementById('prueba').innerHTML = "te sientes: " + cojer;
-
-}
+}*/
