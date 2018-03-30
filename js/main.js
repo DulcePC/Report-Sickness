362558908c53ae85d20bccab6f2a84c494709578
Dulce <dulcefrancelispc@gmail.com>
2018-03-29 22:16:51 -0400

    Chekbox delete, range and tags add

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 $(document).ready(function() {
   $('.datepicker').datepicker();
   $('.timepicker').timepicker();
+  $('.chips').chips();
+});
+$('.chips-autocomplete').chips({
+  placeholder: "Sintomas aqui:",  
+  autocompleteOptions: {
+    data: {
+      'Apple': null,
+      'Microsoft': null,
+      'Google': null
+    },
+    limit: Infinity,
+    minLength: 1,
+  }
 });
 
+function cambia(){
+var slide=document.getElementsById(test5);
+if(slide === max ){
+document.div.style.backgroundColor=red;
+}else{
+  style.backgroundColor=green;
+}
+} 
 /*function prueba(){
 let cojer = document.getElementsByClassName('range-field').innerHTML;
 document.getElementById('prueba').innerHTML = "te sientes: " + cojer;
